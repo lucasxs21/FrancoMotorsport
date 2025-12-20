1c40ba1309d3c13c5072d5b51f5ef27e09803429
lucasxs21 <lucasfranco147@gmail.com>
2025-12-20 18:48:05 -0300

    Update menu.js

--- public/JS/menu.js
+++ public/JS/menu.js
@@ -1,77 +1,24 @@
 const navContainer = document.getElementById("nav-container");
 
+
 navContainer.innerHTML = `
 <nav style="
-    /* NAV */
-.main-nav {
     width: 100%;
     height: 120px;
-    background: #000;
+    background: #000000ff;
     display: flex;
     align-items: center;
     justify-content: flex-start;
     padding-left: 20px;
+    margin: 0;           /* Pegado arriba */
     box-sizing: border-box;
-}
-
-/* LOGO RESPONSIVE */
-.nav-logo {
-    height: 100%;          /* se adapta al nav */
-    max-height: 100px;     /* límite visual */
-    width: auto;           /* mantiene proporción */
-    object-fit: contain;
-    cursor: pointer;
-}
-
-/* TABLET */
-@media (max-width: 768px) {
-    .main-nav {
-        height: 80px;
-        padding-left: 12px;
-    }
-
-    .nav-logo {
-        max-height: 60px;
-    }
-}
-
-/* MOBILE CHICO */
-@media (max-width: 480px) {
-    .main-nav {
-        height: 70px;
-    }
-
-    .nav-logo {
-        max-height: 50px;
-    }
-}
-
 ">
     <img src="../fotos/Logo franco op 2 final.png" 
-         alt="Logo">
+         alt="Logo"
+         style="
+            height: 400px;  /* LOGO más grande */
+            object-fit: contain;
+            cursor: pointer;
+         ">
 </nav>
 `;
-
-
-
-// navContainer.innerHTML = `
-// <nav style="
-//     width: 100%;
-//     height: 120px;
-//     background: #000000ff;
-//     display: flex;
-//     align-items: center;
-//     justify-content: flex-start;
-//     padding-left: 20px;
-//     margin: 0;           /* Pegado arriba */
-//     box-sizing: border-box;
-// ">
-//     <img src="../fotos/Logo franco op 2 final.png" 
-//          alt="Logo"
-//          style="
-//             height: 400px;  /* LOGO más grande */
-//             object-fit: contain;
-//             cursor: pointer;
-//          ">
-// </nav>
-// `;
