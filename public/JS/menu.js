89eecff41d56ed272586c126b8da1470baba858a
lucasxs21 <lucasfranco147@gmail.com>
2025-12-20 18:53:10 -0300

    Update menu.js

--- public/JS/menu.js
+++ public/JS/menu.js
@@ -1,6 +1,30 @@
 const navContainer = document.getElementById("nav-container");
 
 
+// navContainer.innerHTML = `
+// <nav style="
+//     width: 100%;
+//     height: 120px;
+//     background: #000000ff;
+//     display: flex;
+//     align-items: center;
+//     justify-content: flex-start;
+//     padding-left: 20px;
+//     margin: 0;           /* Pegado arriba */
+//     box-sizing: border-box;
+// ">
+//     <img src="../fotos/Logo franco op 2 final.png" 
+//          alt="Logo"
+//          style="
+//             height: 400px;  /* LOGO más grande */
+//             object-fit: contain;
+//             cursor: pointer;
+//          ">
+// </nav>
+// `;
+
+
+
 navContainer.innerHTML = `
 <nav style="
     width: 100%;
@@ -13,12 +37,14 @@ navContainer.innerHTML = `
     margin: 0;           /* Pegado arriba */
     box-sizing: border-box;
 ">
-    <img src="../fotos/Logo franco op 2 final.png" 
-         alt="Logo"
-         style="
-            height: 400px;  /* LOGO más grande */
-            object-fit: contain;
-            cursor: pointer;
-         ">
+    <img src="../fotos/Logo franco op 2 final.png"
+     alt="Logo"
+     style="
+        height: clamp(60px, 20vw, 400px);
+        width: auto;
+        object-fit: contain;
+        cursor: pointer;
+     ">
+
 </nav>
 `;
